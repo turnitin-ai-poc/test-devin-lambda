7190b5e10759dda67fe3e6046fe018c70af45c8d
Dibyo Majumdar <dibyo.majumdar@gmail.com>
2022-08-12 16:05:25 -0700

    feat: Incorporate selected options configuration into template.yml

--- local_extensions.py
+++ local_extensions.py
@@ -11,41 +11,71 @@ from jinja2.ext import Extension
 COOKIECUTTER_PATH = pathlib.Path(__file__).parent / "cookiecutter.json"
 
 
-class VariableSubstitutionExtension(Extension):
+class CookiecutterExtension(Extension):
+    def __init__(self, environment):
+        super().__init__(environment)
+
+        with open(COOKIECUTTER_PATH, "r") as f:
+            self.cookiecutter_config: dict = json.load(f)
+
+
+class VariableSubstitutionExtension(CookiecutterExtension):
     DEFAULT_VARIABLE_START_STRING = "__TEMP"
     DEFAULT_VARIABLE_END_STRING = "TEMP__"
-
-    @classmethod
-    def load_variable_substitution_regex(cls) -> t.Pattern[str]:
-        with open(COOKIECUTTER_PATH, "r") as f:
-            cookiecutter_config: dict = json.load(f)
-
-        config = cookiecutter_config.get("_variable_substitution", {})
-        start_str = config.get("variable_start_string",
-                               cls.DEFAULT_VARIABLE_START_STRING)
-        end_str = config.get("variable_end_string",
-                             cls.DEFAULT_VARIABLE_END_STRING)
-        return re.compile(r"{}([\w\d_]*){}".format(start_str, end_str))
 
     def __init__(self, environment):
         super().__init__(environment)
 
-        self.var_subs_regex = self.load_variable_substitution_regex()
+        config = self.cookiecutter_config.get("_variable_substitution", {})
+        start_str = config.get("variable_start_string",
+                               self.DEFAULT_VARIABLE_START_STRING)
+        end_str = config.get("variable_end_string",
+                             self.DEFAULT_VARIABLE_END_STRING)
+        self.var_regex =\
+            re.compile(r"{}([\w\d_]*){}".format(start_str, end_str))
 
     def preprocess(
             self, source: str, name: t.Optional[str],
             filename: t.Optional[str] = None
     ) -> str:
-        if self.var_subs_regex is None:
-            return source
-
         var_start_str = self.environment.variable_start_string
         var_end_str = self.environment.variable_end_string
 
         def replace_variable(match: t.Match) -> str:
             return f"{var_start_str} cookiecutter.{match[1]} {var_end_str}"
 
-        return self.var_subs_regex.sub(replace_variable, source)
+        return self.var_regex.sub(replace_variable, source)
+
+
+class LineStatementSubstitutionExtension(CookiecutterExtension):
+    DEFAULT_LINE_STATEMENT_PREFIX = "//TEMP"
+
+    FALLBACK_ENV_LINE_STATEMENT_PREFIX = "#TEMP"
+
+    def __init__(self, environment):
+        super().__init__(environment)
+
+        if environment.line_statement_prefix is None:
+            environment.line_statement_prefix =\
+                self.FALLBACK_ENV_LINE_STATEMENT_PREFIX
+
+        config = \
+            self.cookiecutter_config.get("_line_statement_substitution", {})
+        line_stat_prefix = config.get("line_statement_prefix",
+                                      self.DEFAULT_LINE_STATEMENT_PREFIX)
+        self.line_statement_regex = \
+            re.compile(r"^\s*{}(.*)$".format(line_stat_prefix), re.MULTILINE)
+
+    def preprocess(
+            self, source: str, name: t.Optional[str],
+            filename: t.Optional[str] = None
+    ) -> str:
+        line_stat_prefix = self.environment.line_statement_prefix
+
+        def replace_line_statement(match: t.Match) -> str:
+            return f"{line_stat_prefix}{match[1]}"
+
+        return self.line_statement_regex.sub(replace_line_statement, source)
 
 
 class OptionSelectionTestExtension(Extension):
