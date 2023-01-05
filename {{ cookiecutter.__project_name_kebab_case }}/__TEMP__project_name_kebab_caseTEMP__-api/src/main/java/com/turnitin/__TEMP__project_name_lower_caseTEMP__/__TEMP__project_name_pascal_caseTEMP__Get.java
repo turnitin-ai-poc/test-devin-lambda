3b79ffc45094ca74a671fa9713085614527d1a7d
Adrian Yacub <ayacub@turnitin.com>
2023-01-05 11:55:55 -0800

    fix name of service variable, create way to create camel-case

--- {{ cookiecutter.__project_name_kebab_case }}/__TEMP__project_name_kebab_caseTEMP__-api/src/main/java/com/turnitin/__TEMP__project_name_lower_caseTEMP__/__TEMP__project_name_pascal_caseTEMP__Get.java
+++ {{ cookiecutter.__project_name_kebab_case }}/__TEMP__project_name_kebab_caseTEMP__-api/src/main/java/com/turnitin/__TEMP__project_name_lower_caseTEMP__/__TEMP__project_name_pascal_caseTEMP__Get.java
@@ -13,24 +13,24 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class __TEMP__project_name_pascal_caseTEMP__Get extends ApiGatewayLambda<Hello> {
 
-    private __TEMP__project_name_pascal_caseTEMP__Service __temp_lower__service;
+    private __TEMP__project_name_pascal_caseTEMP__Service __TEMP__project_name_camel_caseTEMP__Service;
 
     // This constructor is for regular flow
     public __TEMP__project_name_pascal_caseTEMP__Get() {
         this.ctx = TurnitinContext.builder().build();
-        this.__temp_lower__service = new __TEMP__project_name_pascal_caseTEMP__Service(ctx);
+        this.__TEMP__project_name_camel_caseTEMP__Service = new __TEMP__project_name_pascal_caseTEMP__Service(ctx);
     }
 
     // This Constructor is use in tests if you want to mock the context or parts there of.
     public __TEMP__project_name_pascal_caseTEMP__Get(TurnitinContext ctx) {
         this.ctx = ctx;
-        this.__temp_lower__service = new __TEMP__project_name_pascal_caseTEMP__Service(ctx);
+        this.__TEMP__project_name_camel_caseTEMP__Service = new __TEMP__project_name_pascal_caseTEMP__Service(ctx);
     }
 
     @Override
     protected Hello handleMethod() throws Exception {
         String who = ctx.getKeyFromPathParams(input, "who");
-        return __temp_lower__service.doHello(who);
+        return __TEMP__project_name_camel_caseTEMP__Service.doHello(who);
     }
 
     @Override
